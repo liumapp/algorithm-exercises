b874411de0c8133d744dba43841bb593b6716584
liumapp <liumapp.com@gmail.com>
2018-03-06 17:20:56 +0800

    rm unnecessary things

--- algorithm-basic/src/main/java/com/liumapp/algorithm/basic/Basic.java
+++ algorithm-basic/src/main/java/com/liumapp/algorithm/basic/Basic.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/6/18 5:20 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
-public class Main {
+@SpringBootApplication
+public class Basic {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(Basic.class , args);
     }
 
 }
