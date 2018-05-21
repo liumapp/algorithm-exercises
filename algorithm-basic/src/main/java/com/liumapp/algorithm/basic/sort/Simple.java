4d2e408dc1d626d0775f95ad881b6c048e7c3fa3
liumapp <liumapp.com@gmail.com>
2018-05-21 18:52:50 +0800

    ready to add sort algo

---
+++ algorithm-basic/src/main/java/com/liumapp/algorithm/basic/sort/Simple.java
@@ -0,0 +1,17 @@
+package com.liumapp.algorithm.basic.sort;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liumapp
+ * @file Simple.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/21/18
+ */
+@Component
+public class Simple {
+
+
+
+}
---
+++ algorithm-basic/src/test/java/com/liumapp/algorithm/basic/sort/SimpleTest.java
@@ -0,0 +1,14 @@
+package com.liumapp.algorithm.basic.sort;
+
+import com.liumapp.algorithm.basic.Basic;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {Basic.class})
+public class SimpleTest {
+
+    
+
+}
