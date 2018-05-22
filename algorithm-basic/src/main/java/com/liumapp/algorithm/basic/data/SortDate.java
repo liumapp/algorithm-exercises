e2bef6a05350734292deb6d399b64154338d06c8
liumapp <liumapp.com@gmail.com>
2018-05-22 16:29:18 +0800

    get sort date done

--- algorithm-basic/src/main/java/com/liumapp/algorithm/basic/data/SortDate.java
+++ algorithm-basic/src/main/java/com/liumapp/algorithm/basic/data/SortDate.java
@@ -11,12 +11,49 @@ import java.util.Date;
  * @homepage http://www.liumapp.com
  * @date 5/21/18
  */
-@Component
-public class SortDate implements Comparable<Date> {
+public class SortDate implements Comparable<SortDate> {
+
+    private final int day;
+
+    private final int month;
+
+    private final int year;
+
+    public SortDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
 
     @Override
-    public int compareTo(Date o) {
+    public int compareTo(SortDate o) {
+        if (this.year > o.getYear()) return +1;
+        if (this.year < o.getMonth()) return -1;
+        if (this.month > o.getMonth()) return +1;
+        if (this.month < o.getMonth()) return -1;
+        if (this.day > o.getDay()) return +1;
+        if (this.day < o.getDay()) return -1;
         return 0;
     }
 
+    @Override
+    public String toString() {
+        return "SortDate{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
 }
