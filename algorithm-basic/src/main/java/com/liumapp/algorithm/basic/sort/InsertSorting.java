c23015ad91d7ed9a95528a1dbe871e2d62aaf0ac
liumapp <liumapp.com@gmail.com>
2018-05-30 19:23:38 +0800

    ready to add insert sorting

---
+++ algorithm-basic/src/main/java/com/liumapp/algorithm/basic/sort/InsertSorting.java
@@ -0,0 +1,14 @@
+package com.liumapp.algorithm.basic.sort;
+
+/**
+ * @author liumapp
+ * @file InsertSorting.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/30/18
+ */
+public class InsertSorting extends Example {
+
+
+
+}
